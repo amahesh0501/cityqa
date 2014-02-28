c09983114d6b5198c1524b28fca91de5fb794764
Apprentice <apprentice@DBC-Infinity-2.local>
2014-02-27 23:01:44 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cityqa::Application.config.secret_token = '0afb2152a2dc6cb18d536e045a0a17d8b4b05bbcca6860ffb8d738e15a22c9f1353cd8a898a35273a160a0352d24a72cbe9a8490ae5da76c54f287d5af64e892'
